1ae0602c038d43154353c9e993c67ddce405c704
Ian Wagner <ian.wagner@stadiamaps.com>
2023-01-31 02:34:12 +0900

    Initialize project using Create React App

---
+++ src/App.js
@@ -0,0 +1,22 @@
+import "./App.css";
+import { MapLibreMap, MlNavigationTools } from "@mapcomponents/react-maplibre";
+
+import "maplibre-gl/dist/maplibre-gl.css";
+
+function App() {
+  return (
+    <>
+      <MapLibreMap
+        mapId="map_1"
+        options={{
+          zoom: 8,
+          style: "https://wms.wheregroup.com/tileserver/style/osm-bright.json",
+          center: [7.0851268, 50.73884],
+        }}
+      />
+      <MlNavigationTools mapId="map_1" />
+    </>
+  );
+}
+
+export default App;
